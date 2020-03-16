a78a9c2c7ffc2cf6ac82ea16c1b292e93784e9a6
CoreyDeJong <dejongcorey@gmail.com>
2020-03-15 18:33:36 -0700

    updated files

--- js/app.js
+++ js/app.js
@@ -1,81 +1,60 @@
 'use strict'
 
-//array with all instances of animals from the page-1.json file
-let animalArray = [];
-
-function Gallery (obj){
-    this.image = obj.image_url;
-    this.title = obj.title;
-    this.description = obj.description;
-    this.keyword = obj.keyword;
-    this.horns = obj.horns;
-    animalArray.push(this);
-}
-
-
-Gallery.prototype.renderAnimal = function () {
-   //#photo-template is the id in the section in html. ".html()" will get the html within the photo-template section
-    let template = $('#photo-template').html()
-
-    //make a new section. "$section" is just a way to name, the $ doesn't trigger jQuery
-    let $section = $('<section></section>');
-
-    // will put the html structure defined in the template variable into a new $section html code
-    $section.html(template);
-
-    //find the header in the html and assign it the title from the constructor
-    $section.find('h2').text(this.title);
-    $section.find('p').text(this.description);
-    $section.find('img').attr('src', this.image);
-
-    // append section to the main section in index.html
-    $('main').append($section);
-}
-
-
-  // $.ajax to get the information from the page-1.json file
-  $.ajax('./data/page-1.json', {METHOD: 'GET', DATATYPE: 'JSON'})
+$.ajax('data/page-1.json', {method: 'GET', dataType: 'JSON'})
   .then(data => {
-    //   console.log(data);
     data.forEach(animal => {
-        new Gallery(animal).renderAnimal()
-        filter();
+      new Animal(animal).render()
+      filter();
     })
     renderKeyword();
-    clickEvent();
   })
 
 
+let animalArray = [];
 
-  // need an array of unique key words
-  // use that array to generate the drop down list
-  // when the user selects a name, filter the images that are displayed.
+function Animal(obj){
+  this.title = obj.title;
+  this.image_url=obj.image_url;
+  this.description = obj.description;
+  this.horns = obj.horns;
+  this.keyword = obj.keyword;
+  animalArray.push(this);
+}
 
+Animal.prototype.render = function(){
+  // select all the html in the template
+  const myTemplate = $('#photo-template').html();
 
-// create an array that stores keywords "unicorn, narwhal,..." without duplicates.
+  // make a new section
+  const $newSection = $('<section></section>');
+  $newSection.html(myTemplate);
+  $newSection.find('h2').text(this.title);
+  $newSection.find('img').attr('src', this.image_url);
+  $newSection.find('p').text(this.description);
+  $newSection.attr('keyword', this.keyword);
+  $('main').append($newSection);
+
+}
+
+//keyword options
 const newOptionArray = [];
 
-//function will take in the animalArray that has all instances of animals and filter to only unique keywords into the newOptionArray
 function filter (){
-  animalArray.sort().forEach(keys => {
+  animalArray.forEach(keys => {
     if (!newOptionArray.includes(keys.keyword)){
       newOptionArray.push(keys.keyword);
     }
   })
 }
 
-//go through each keyword in the newOptionArray and append the keyword into the drop-down option in the html using jquery
 function renderKeyword() {
-  
   const $option = $('select')
   newOptionArray.forEach(keyword => {
     const $newOption = $(`<option value:"${keyword}">${keyword}</option>`);
     $option.append($newOption);
-      });
+  });
 }
 
-
-// when the user clicks on a drop-down option
 function clickEvent(event) {
   const sect = $('section');
   sect.each((index, value)=>{
--- js/app2.js
+++ js/app2.js
@@ -1,63 +1,36 @@
-
 'use strict'
 let getTemplate = $('#temp').html();
 let templateRender = Handlebars.compile(getTemplate);
 
-//array with all instances of animals from the page-1.json file
-let animalArray = [];
-
-function Gallery (obj){
-    this.image = obj.image_url;
-    this.title = obj.title;
-    this.description = obj.description;
-    this.keyword = obj.keyword;
-    this.horns = obj.horns;
-    animalArray.push(this);
-}
-
-
-Gallery.prototype.renderAnimal = function () {
-   //#photo-template is the id in the section in html. ".html()" will get the html within the photo-template section
-    let template = $('#photo-template').html()
-
-    //make a new section. "$section" is just a way to name, the $ doesn't trigger jQuery
-    let $section = $('<section></section>');
-
-    // will put the html structure defined in the template variable into a new $section html code
-    $section.html(template);
-
-    //find the header in the html and assign it the title from the constructor
-    $section.find('h2').text(this.title);
-    $section.find('p').text(this.description);
-    $section.find('img').attr('src', this.image);
-
-    // append section to the main section in index.html
-    $('main').append($section);
-}
-
-
-  // $.ajax to get the information from the page-1.json file
-  $.ajax('./data/page-2.json', {METHOD: 'GET', DATATYPE: 'JSON'})
+$.ajax('data/page-2.json', {method: 'GET', dataType: 'JSON'})
   .then(data => {
-    //   console.log(data);
     data.forEach(animal => {
-        new Gallery(animal).renderAnimal()
-        filter();
-    })
+      let newAnimal = new Animal(animal);
+      newAnimal.renderTemplate();
+      filter();
+    });
     renderKeyword();
   })
 
+let animalArray = [];
+
+function Animal(obj){
+  this.title = obj.title;
+  this.image_url = obj.image_url;
+  this.description = obj.description;
+  this.horns = obj.horns;
+  this.keyword = obj.keyword;
+  animalArray.push(this);
+}
 
 
-  // need an array of unique key words
-  // use that array to generate the drop down list
-  // when the user selects a name, filter the images that are displayed.
+Animal.prototype.renderTemplate= function(){
+  $('#photo-template').append(templateRender(this));
+}
 
-
-// create an array that stores keywords "unicorn, narwhal,..." without duplicates.
+//keyword options
 const newOptionArray = [];
 
-//function will take in the animalArray that has all instances of animals and filter to only unique keywords into the newOptionArray
 function filter (){
   animalArray.forEach(keys => {
     if (!newOptionArray.includes(keys.keyword)){
@@ -66,20 +39,16 @@ function filter (){
   })
 }
 
-//go through each keyword in the newOptionArray and append the keyword into the drop-down option in the html using jquery
 function renderKeyword() {
-  
   const $option = $('select')
   newOptionArray.forEach(keyword => {
     const $newOption = $(`<option value:"${keyword}">${keyword}</option>`);
     $option.append($newOption);
-      });
+  });
 }
 
-
-// when the user clicks on a drop-down option
 function clickEvent(event) {
-  const sect = $('section');
+  const sect = $('div');
   sect.each((index, value)=>{
     if( $(value).attr('keyword') === event.target.value){
       console.log(value);
@@ -91,5 +60,6 @@ function clickEvent(event) {
   )
 }
 $('select').change(clickEvent);
+
 
 $(function() {});
